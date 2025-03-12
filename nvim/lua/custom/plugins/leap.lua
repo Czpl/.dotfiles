1272105ffe02276ecf6ae9f434f2c78bd3a6c5aa
Mateusz Czapla <diabol111@gmail.com>
2025-03-12 14:55:17 +0100

    initial commit

---
+++ nvim/lua/custom/plugins/leap.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'ggandor/leap.nvim',
+    opts = {
+      vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)'),
+      vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)'),
+      vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)'),
+    },
+  },
+}
