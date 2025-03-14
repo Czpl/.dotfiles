0b3f0a9dc5225b8ed4532c7d5b0499adb31b9808
Mateusz Czapla <diabol111@gmail.com>
2025-03-14 14:54:06 +0100

    update nvim/init.lua, tmux.conf

--- nvim/init.lua
+++ nvim/init.lua
@@ -679,7 +679,20 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = '<C-s>a', -- Add surrounding in Normal and Visual modes
+          delete = '<C-s>d', -- Delete surrounding
+          find = '<C-s>f', -- Find surrounding (to the right)
+          find_left = '<C-s>F', -- Find surrounding (to the left)
+          highlight = '<C-s>h', -- Highlight surrounding
+          replace = '<C-s>r', -- Replace surrounding
+          update_n_lines = '<C-s>n', -- Update `n_lines`
+
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
@@ -690,7 +703,7 @@ require('lazy').setup({
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
+      -- cursor location to LINE:COLUMNM
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
         return '%2l:%-2v'
